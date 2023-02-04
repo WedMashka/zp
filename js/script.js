2e4f44def95a0a3940df4fade3e1a81f1cd27118
ionasmith <ionasmith@yandex.ru>
2023-02-04 10:24:43 +0300

    ,2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 "use strict"
-function s(x) {
-   console.log(x);
-}
-function sh(r) {
-   let result = r;
-   document.getElementById('result').innerHTML = result;
-}
-function showPecent() {
-   let summa1 = Number(document.getElementById('valueSumm').value);
-   let pecentOfYears = Number(document.getElementById('valuePecent').value);
-   let timeInt = Number(document.getElementById('valueInterval').value);
-   let xx;
-   let xxx;
-   xxx = ((summa1 * (pecentOfYears / 100)) / 365) * timeInt;
-   if (isNaN(xxx)) {
-      xx = "Пожалуйста введите цифры";
-   } else {
-      xx = xxx;
-   } 
-   return document.getElementById('resultSumm').innerHTML = xx;
+
+
+
+((basePrem * 0.6 * kpiKachestva) + (basePrem * 0.2 * kpiSglaschiv) + (basePrem * 0.2 * tainiKlient))
+
+((basePrem * 0.7 * kpiKachestva) + (basePrem * 0.3 * kpiSglaschiv))
+
+(basePrem * reshenieVoprosa) + (basePrem * transfer) + (basePrem * csat)
+
+function count() {
+      let basePrem = Number(document.getElementById('basePremiumRate').value);
+
+      let kpiKachestva = Number(document.getElementById('kpiKachestva').value);
+
+      let kpiSglaschiv = Number(document.getElementById('kpiSglaschivaniya').value);
+
+      let tainiKlient = Number(document.getElementById('tainiKlient').value);
+
+      let csat = Number(document.getElementById('csat').value);
+
+      let reshenieVoprosa = Number(document.getElementById('reshenieVoprosa').value);
+
+      let transfer = Number(document.getElementById('transfer').value);
+
+      let result = document.getElementById('resultPrem').innerHTML;
+
+   let x = ((basePrem * 0.6 * kpiKachestva) + (basePrem * 0.2 * kpiSglaschiv) + (basePrem * 0.2 * tainiKlient));
+   let p =
+
+
 }
